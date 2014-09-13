bda2ba03ee41bbb56fd38c75d6705e65c6ee5e44
unknown <elliotlimzy@gmail.com>
2014-09-13 23:22:29 +0800

    Added Icons and Lang Registry

--- source/me/spyobird/encumberment/lib/ModPotion.java
+++ source/me/spyobird/encumberment/lib/ModPotion.java
@@ -9,7 +9,7 @@ import cpw.mods.fml.relauncher.SideOnly;
 public class ModPotion extends Potion
 {
 	public static Potion encumbered = new ModPotion(33, true, 16717021).setPotionName("encumberment.potionEncumbered").setIconIndex(0, -11);
-	public static Potion vitality = new ModPotion(34, true, 16130975).setPotionName("encumberment.potionVitality").setIconIndex(1, -11);
+	public static Potion vitality = new ModPotion(34, true, 16130975).setPotionName("encumberment.potionVitality").setIconIndex(0, -10);
 	
 	public ModPotion(int id, boolean isBad, int colourMult)
     {
